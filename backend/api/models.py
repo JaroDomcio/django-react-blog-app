310f21adfe7456e769e077818bfb14556da184f8
JaroDomcio <281053@student.pwr.edu.pl>
2025-07-02 21:57:15 +0200

    Added post model and easy api post/get

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=200)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        unique_together = (('title', 'slug'),)
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.title.lower().replace(' ', '-')
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from rest_framework import generics 
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
